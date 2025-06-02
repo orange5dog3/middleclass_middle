7006ee6458485e4feaef71d0624f0fbaae7e070e
orange5dog3 <orange5dog3@gmail.com>
2025-06-02 14:56:58 +0900

    arrow途中

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$('.slick').slick({
+    autoplay: true, 
+    fade: true, 
+    cssEase: 'linear',
+    speed: 2000, 
+    arrows:true,
+    prevArrow: '<button type="button" class="slide-arrow-prev">←</button>',
+    nextArrow: '<button type="button" class="slide-arrow-next">→</button>',
+    dots:true,
+    dotsClass: "slide-dots"
+});
+
